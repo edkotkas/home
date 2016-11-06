d9a5cc1c14ec8271a483b7f689f6b54cdfdf858f
Eduard Kotkas <edkotkas@gmail.com>
2016-11-06 18:24:01 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,117 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+
+  @Output() blockClicker = new EventEmitter<any>()
+
+  private cross: string = ' cross'
+  private oval: string = ' oval'
+  private symbol: string
+
+  private gridSize: number = 3
+
+  private blocks = Array(9).fill("")
+  private filledBlocks = Array(9).fill("")
+
+  private maxHeight = window.innerHeight
+  private maxWidth = window.innerWidth
+
+  constructor() {
+    if (this.blocks.length != this.getBlocks()){
+      this.blocks = Array(this.getBlocks()).fill("")
+      this.filledBlocks = Array(this.getBlocks()).fill("")
+    }
+  }
+
+  ngOnInit() {
+    this.blockClicker.subscribe(event => {
+      if(this.gameWon() === false
+        && !this.filledBlocks[event.block].includes(this.cross)
+        && !this.filledBlocks[event.block].includes(this.oval)) {
+
+        this.swapSymbol()
+        this.filledBlocks[event.block] = this.symbol
+      }
+
+      if (this.gameWon() === true) {
+        this.filledBlocks = this.filledBlocks.map(f =>  f === this.symbol ? f += ' win' : f)
+      }
+      console.log(this.filledBlocks)
+    })
+  }
+
+  keyUp(event) {
+    if(event.code === 'KeyR') {
+      this.reset()
+    }
+  }
+
+  reset() {
+    this.blocks = Array(this.getBlocks()).fill("")
+    this.filledBlocks = Array(this.getBlocks()).fill("")
+  }
+
+  getBlocks() {
+    return this.gridSize * this.gridSize
+  }
+
+  gameWon() {
+    let horiz: any = []
+    let verti: any = []
+    for(let i = 0; i < this.gridSize; i++) {
+      for (let j = 0; j < this.gridSize ; j++) {
+        horiz.push(this.checkFilled(i * this.gridSize + j))
+        verti.push(this.checkFilled(j * this.gridSize + i))
+      }
+      if (!horiz.includes(false) || !verti.includes(false)) {
+        return true
+      }
+    }
+
+    let diagOne: any = []
+    for(let i = 0; i < this.getBlocks(); i+=4) {
+        diagOne.push(this.checkFilled(i))
+    }
+
+    let diagTwo: any = []
+    for(let i = 2; i < this.getBlocks(); i+=2) {
+        diagTwo.push(this.checkFilled(i))
+    }
+
+    if (!diagOne.includes(false)|| !diagTwo.includes(false)) {
+      return true
+    }
+
+    return false
+  }
+
+  checkFilled(i) {
+    return this.filledBlocks[i].includes(this.symbol)
+  }
+
+  onResize() {
+    this.maxWidth = window.innerWidth
+    this.maxHeight = window.innerHeight
+  }
+
+  getMaxSymbolSize() {
+    let min = this.maxWidth < this.maxHeight
+      ? this.maxWidth : this.maxHeight
+
+    return (min - (min / 3)) / 3
+  }
+
+  swapSymbol() {
+    this.symbol = this.symbol == this.cross ? this.oval : this.cross
+  }
+
+  clickBlock(event) {
+    this.blockClicker.emit(event)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -17,4 +17,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
